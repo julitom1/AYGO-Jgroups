4d2f39e665d7f343f2f7a191e6a65748aa05a719
yarit villalobos <yaritvillalobos31@gmail.com>
2024-10-26 09:37:10 -0500

    jgroup

---
+++ src/test/java/edu/eci/jgroups/JgroupsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.jgroups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgroupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
